2f6bcbcf94e842622c10c147eae3209e23bf64d6
Prakash <rssprakash99@gmail.com>
2020-07-14 19:28:47 +0530

    This is Properties file

---
+++ src/test/java/one/Properties_file.java
@@ -0,0 +1,11 @@
+package one;
+
+public class Properties_file {
+	
+	public static void main(String args[]) {
+		System.out.println("This is Properties file");
+		
+		System.out.println("This is runner file");
+	}
+
+}
